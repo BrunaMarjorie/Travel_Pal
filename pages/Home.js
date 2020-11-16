4336f842f95c1fe58f50cf3bbd9d801e6b1b4793
Bruna Santana <brunamarjorie@gmail.com>
2020-11-16 20:35:36 +0000

    permissions

--- pages/Home.js
+++ pages/Home.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
+import * as Location from 'expo-location';
 
 export default function App() {
+    const [location, setLocation] = useState(null);
+    const [errorMsg, setErrorMsg] = useState(null);
+
+    useEffect(() => {
+        (async () => {
+            let { status } = await Location.requestPermissionsAsync();
+            if(status !== "granted"){
+                setErrorMsg('Permision to location was denied');
+            }
+
+            let location = await Location.getCurrentPositionAsync({});
+            setLocation(location);
+        })
+    })
+
+    let text = 'Waiting...';
+    if(errorMsg){
+        text = errorMsg;
+    }else if (location) {
+        text = JSON.stringify(location);
+    }
+
     return (
         <View style={styles.container}>
-            <Text>Home</Text>
+            <Text>{text}</Text>
         </View>
     );
 }
